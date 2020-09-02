6ddf5fbeee877d9f4bc6b9c21c778a4ac21bdd54
kirirotha <kirirotha@gmail.com>
2020-09-02 15:03:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+const findMatching = (drivers, string) =>{
+    let selectedDrivers = drivers.filter(x =>{
+        return x.toUpperCase() === string.toUpperCase()
+    })
+    return selectedDrivers
+}
+
+
+const fuzzyMatch = (drivers, string) => {
+    let stringLength = string.length
+    let selectedDrivers = drivers.filter(x => {
+        return x.slice(0, stringLength) === string
+    })
+    return selectedDrivers
+}
+
+const matchName = (drivers, string) => {
+    let selectedDrivers = drivers.filter(x => {
+        debugger
+        return x.name === string
+    })
+    return selectedDrivers
+}
